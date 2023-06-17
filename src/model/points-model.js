44d992296cfa1aa9bdb904b96565b6659d6acedc
myashaa <mianto017@mail.ru>
2023-06-17 13:57:06 +0300

    Использует паттерн Наблюдатель.

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,7 +1,8 @@
 import {POINT_COUNT} from '../const.js';
 import {getRandomPoint} from '../mock/point.js';
+import Observable from '../framework/observable.js';
 
-export default class PointsModel {
+export default class PointsModel extends Observable {
   #points = Array.from({ length: POINT_COUNT }, getRandomPoint);
 
   get points() {
